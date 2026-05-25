bb52402725f0b55c598d4fc0b01b56e28d80b411
Bassem_Fullstack <bassemali102345@gmail.com>
2026-05-25 18:54:22 +0300

    sssssssssssssssssss

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,24 @@
+
+
 const mongoose = require("mongoose")
 
-let cached = global.mongoose
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null }
+const connectDB = async ()=> {
+
+try{
+
+await mongoose.connect(process.env.DB_URL)
+
+console.log("MongoDB Connected")
 }
 
-const connectDB = async () => {
-  if (cached.conn) return cached.conn
+catch(e){
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.DB_URL).then((m) => m)
-  }
+console.log(e.message)
 
-  cached.conn = await cached.promise
-  return cached.conn
 }
+
+} 
+
 
 module.exports = connectDB
